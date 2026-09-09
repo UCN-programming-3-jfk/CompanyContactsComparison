3244c55cb502d52e11c153abc36d2ac2c908fb3b
Jakob Farian Krarup <kr@rup.dk>
2026-09-09 13:59:00 +0200

    x and y changed to first and second

--- CompanyContacts.DAL/Comparers/CompanyIdComparer.cs
+++ CompanyContacts.DAL/Comparers/CompanyIdComparer.cs
@@ -4,6 +4,9 @@ internal class CompanyIdComparer : IComparer<Company>
 {
     public int Compare(Company? first, Company? second)
     {
+        if (ReferenceEquals(first, second)) { return 0; }
+        if (first is null) { return -1; }
+        if (second is null) { return 1; }
         return first.Id.CompareTo(second.Id);
     }
 }
